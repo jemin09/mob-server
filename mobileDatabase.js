52a7a8c4c8fd2206f3295451f307be2df767b4db
Jesmin Baidya <jesminbaidya123@gmail.com>
2023-02-09 10:19:28 +0530

    commmgfh

--- mobileDatabase.js
+++ mobileDatabase.js
@@ -1,12 +1,13 @@
 let mysql=require("mysql");
 let connData={
-  host:"localhost",
+  host:"127.0.0.1",
   user:"root",
   password:"",
   database:"employeedb"
 };
 
 function getConnection(){
+  console.log("Connect!!");
   return mysql.createConnection(connData);
 };
 module.exports.getConnection=getConnection;
